c1d8711c85b4b43bf9d840d9be0adfbb2bfed9ee
Rick Waldron <rickwaldron7@gmail.com>
2024-04-19 21:03:32 +1000

    Reverted to previous

--- app/core/Model.php
+++ app/core/Model.php
@@ -5,7 +5,7 @@ namespace App\Core;
 class Model
 {
     protected static $conn = null;
-
+    
     public function __construct()
     {
         if(self::$conn == null)
@@ -13,23 +13,17 @@ class Model
             try 
             {
                 // Connection information
-                $host = "localhost";
-                $user = "root";
-                $pw = "";
-                $db = "markr";
+                $host = HOST;
+                $user = USER;
+                $pw = PW;
+                $db = DB;
                 // Connect to DB
                 self::$conn = new \PDO("mysql:host=$host;dbname=$db",$user,$pw);
             }
             catch (\PDOException $e)
             {
-                // Connection information
-                $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-                $host = $url["host"];
-                $user = $url["user"];
-                $pw = $url["pass"];
-                $db = substr($url["path"], 1);
-                // Connect to DB
-                self::$conn = new \PDO("mysql:host=$host;dbname=$db",$user,$pw);          
+                print("Error connecting to SQL Server.");
+                die(print_r($e));          
             }
             
         }
--- app/init.php
+++ app/init.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'config/db_connection.php';
 require_once 'core/App.php';
 require_once 'core/Controller.php';
 require_once 'core/Model.php';
